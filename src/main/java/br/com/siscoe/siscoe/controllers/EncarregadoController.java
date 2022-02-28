7b18978b73da7b7b44bbd3e9bc220abb896da9c1
EricEOL <lopes.eric051@gmail.com>
2022-02-28 13:34:16 -0300

    Método list em EncarregadoController

--- src/main/java/br/com/siscoe/siscoe/controllers/EncarregadoController.java
+++ src/main/java/br/com/siscoe/siscoe/controllers/EncarregadoController.java
@@ -3,15 +3,22 @@ package br.com.siscoe.siscoe.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siscoe.siscoe.entities.Building;
 import br.com.siscoe.siscoe.entities.Encarregado;
+import br.com.siscoe.siscoe.entities.dto.BuildingDTO;
 import br.com.siscoe.siscoe.entities.form.encarregado.EncarregadoForm;
 import br.com.siscoe.siscoe.repositories.EncarregadoRepository;
 
@@ -21,6 +28,19 @@ public class EncarregadoController {
 
 	@Autowired
 	EncarregadoRepository encarregadoRepository;
+	
+	@GetMapping
+	public Page<Encarregado> list(@RequestParam(required = false) String cia, 
+			@PageableDefault(sort = "number", direction = Direction.ASC, page = 0, size = 10) Pageable pageable) {
+		
+		if(cia == null) {
+			Page<Encarregado> encarregados = encarregadoRepository.findAll(pageable);
+			return encarregados;
+		} else {
+			Page<Encarregado> encarregados = encarregadoRepository.findByCia(cia, pageable);
+			return encarregados;
+		}
+	}
 	
 	@PostMapping
 	@Transactional
--- src/main/java/br/com/siscoe/siscoe/repositories/EncarregadoRepository.java
+++ src/main/java/br/com/siscoe/siscoe/repositories/EncarregadoRepository.java
@@ -2,6 +2,8 @@ package br.com.siscoe.siscoe.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +15,7 @@ public interface EncarregadoRepository extends JpaRepository<Encarregado, Long>{
 	public Optional<Encarregado> findByName(String name);
 	
 	public Encarregado getByName(String name);
+
+	public Page<Encarregado> findByCia(String cia, Pageable pageable);
 	
 }
