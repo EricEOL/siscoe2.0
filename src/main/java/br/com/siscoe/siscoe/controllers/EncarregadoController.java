2634477a604c9dcb228f9d5b6b8f94dc49445dc6
EricEOL <lopes.eric051@gmail.com>
2022-02-28 13:31:01 -0300

    Mudança na resposta do método add no EncarregadoController

--- src/main/java/br/com/siscoe/siscoe/controllers/EncarregadoController.java
+++ src/main/java/br/com/siscoe/siscoe/controllers/EncarregadoController.java
@@ -3,12 +3,15 @@ package br.com.siscoe.siscoe.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siscoe.siscoe.entities.Encarregado;
 import br.com.siscoe.siscoe.entities.form.encarregado.EncarregadoForm;
 import br.com.siscoe.siscoe.repositories.EncarregadoRepository;
 
@@ -21,8 +24,9 @@ public class EncarregadoController {
 	
 	@PostMapping
 	@Transactional
-	public void add(@RequestBody @Valid EncarregadoForm encarregadoForm) {
-		encarregadoRepository.save(encarregadoForm.transform());
+	public ResponseEntity<Encarregado> add(@RequestBody @Valid EncarregadoForm encarregadoForm) {
+		Encarregado encarregado = encarregadoRepository.save(encarregadoForm.transform());
+		return ResponseEntity.created(null).body(encarregado);
 	}
 	
 }
